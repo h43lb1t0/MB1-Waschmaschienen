12a8f8d845fa52d5797a653bde50dfae0036f8e5
Tom Haelbich <65122811+h43lb1t0@users.noreply.github.com>
2024-11-12 21:22:18 +0100

    added eventlet to run websocket with gunicorn

--- mb1.sh
+++ mb1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-gunicorn -w 2 -b 0.0.0.0:5000 app:app --log-file /home/notroot/MB1-Waschmaschienen/gunicorn.log
+gunicorn -w 1 --worker-class eventlet -b 0.0.0.0:5000 app:app --log-file /home/notroot/MB1-Waschmaschienen/gunicorn.log
